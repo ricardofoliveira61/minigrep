d0368f6965fe2fc861e4b2cedf4c9e013fd0ea60
Ricardo Oliveira <ricardofoliveira61@gmail.com>
2026-07-11 20:37:17 +0100

    [FEAT] Add cli config

---
+++ src/cli.rs
@@ -0,0 +1,25 @@
+// display line number
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "minigrep")]
+#[command(about = "Search patterns within files")]
+pub struct Arguments {
+    pub pattern: String,
+
+    #[arg(required = true)]
+    pub paths: Vec<String>,
+
+    #[arg(short = 'i', long = "ignore-case")]
+    pub ignore_case: bool,
+
+    #[arg(short = 'r', long = "recursive")]
+    pub recursive: bool,
+
+    #[arg(short = 'c', long = "count")]
+    pub count: bool,
+
+    #[arg(short = 'l', long = "show-line", default_value_t = true)]
+    pub line_number: bool,
+}
